5b7fcf138074a76ad075d39324f280fca34fe023
Titian Cernicova-Dragomir <dragomirtitian@yahoo.com>
2019-01-02 23:36:29 +0200

    Added inherited protected members

--- lowclass.ts
+++ lowclass.ts
@@ -8,25 +8,30 @@ type ConstructorOrDefault<T> = T extends { constructor: infer TCtor } ? TCtor : 
 type CreateInstanceType<T> = Pick<T, Exclude<keyof T, 'constructor' | ImplementationKeys>>;
 type Id<T> = {} & { [P in keyof T]: T[P] }
 type Constructor<T =  any, A extends any[]= any[]> = new (...a: A) => T
-type MakeSuper<T extends Constructor<any>> = T extends Constructor<infer I, infer A> ? { constructor: (...a: A) => I } & Id<Pick<T, keyof T>> : never;
+type MakeSuper<T extends Constructor<any>> = T extends Constructor<infer I, infer A> ? { constructor: (...a: A) => I } & Id<Pick<InstanceType<T>, keyof InstanceType<T>>> : never;
 
 type ImplementationKeys = 'static' | 'private' | 'protected';
 type MakePrivate= <T> (self: T) =>  T extends { __: { private: infer TPrivate } } ? TPrivate : never;
 type MakePublic = <T>(self: T) => Pick<T, Exclude<keyof T, 'constructor' | ImplementationKeys>>;
 type MakeProtected =<T> (self: T) => T extends { __: { protected : infer TProtected }  }? TProtected : never;
-type GetStatics<T> = T extends { static: infer TStatic } ? TStatic : {};
+type Statics<T> = T extends { static: infer TStatic } ? TStatic : {};
+type InheritedProtected<T> = T extends { protected: infer TProtected } ? TProtected : {};
+type StaticsAndProtected<T> = Id<Statics<T> & {
+	__: { protected: InheritedProtected<T> }
+}>
+type ExtractInheritedProtected<T> =  T extends { __: infer TProtected } ? TProtected : {};
 type _PrivateImplementation<T> = Pick<T, Extract<keyof T, ImplementationKeys>>
 type LowClassThis<T> = Id< Pick<T, Exclude<keyof T, ImplementationKeys>> & { __: _PrivateImplementation<T>}>
 
 function Class(name: string): {
-	extends<TBase extends Constructor, T>(base: TBase, members: (b: { Super: (t: any) => MakeSuper<TBase>, Public: MakePublic, Protected: MakeProtected, Private: MakePrivate }) => T & ThisType<LowClassThis<T & InstanceType<TBase>>>):
+	extends<TBase extends Constructor, T>(base: TBase, members: (b: { Super: (t: any) => MakeSuper<TBase>, Public: MakePublic, Protected: MakeProtected, Private: MakePrivate }) => T & ThisType<LowClassThis<T & InstanceType<TBase> & ExtractInheritedProtected<TBase>>>):
 		T extends { constructor: infer TCtor } ?
-		FunctionToConstructor<ConstructorOrDefault<T>, Id<InstanceType<TBase> & CreateInstanceType<T>>>
+		FunctionToConstructor<ConstructorOrDefault<T>, Id<InstanceType<TBase> & CreateInstanceType<T>>> & Id<StaticsAndProtected<T> & Pick<TBase, keyof TBase>>
 		:
-		ReplaceCtorReturn<TBase, InstanceType<TBase> & Pick<T, Exclude<keyof T, 'constructor'>>>
+		ReplaceCtorReturn<TBase, Id<InstanceType<TBase> & Pick<T, Exclude<keyof T, 'constructor'>>>> & Id<StaticsAndProtected<T> & Pick<TBase, keyof TBase>>
 }
-function Class<T>(name: string, membersFn: (b: { Public: MakePublic, Protected: MakeProtected, Private: MakePrivate, Super: never }) => T & ThisType<LowClassThis<T>>): FunctionToConstructor<ConstructorOrDefault<T>, Id<CreateInstanceType<T>>> & Id<GetStatics<T>>
-function Class<T>(name: string, members: T & ThisType<LowClassThis<T>>): FunctionToConstructor<ConstructorOrDefault<T>, Id<CreateInstanceType<T>>>  
+function Class<T>(name: string, membersFn: (b: { Public: MakePublic, Protected: MakeProtected, Private: MakePrivate, Super: never }) => T & ThisType<LowClassThis<T>>): FunctionToConstructor<ConstructorOrDefault<T>, Id<CreateInstanceType<T>>> & Id<StaticsAndProtected<T>>
+function Class<T>(name: string, members: T & ThisType<LowClassThis<T>>): FunctionToConstructor<ConstructorOrDefault<T>, Id<CreateInstanceType<T>>> & Id<StaticsAndProtected<T>>
 function Class(): any {
 	return null as any;
 }
@@ -107,8 +112,6 @@ const Foo = Class('Foo', ({ Public, Protected, Private, Super }) => ({
 		Private(this).privateMethod()
 		console.log(Private(this).privateProp) // 789
 		return Private(this).privateProp;
-
-		
 	},
 
 }));
@@ -118,9 +121,34 @@ let foo = new Foo("")
 Foo.staticMethod();
 Foo.staticProp;
 
-const Bar = Class('Foo').extends(Foo, ({ Public, Protected, Private, Super }) => ({
-	method(){
-		Protected(this);
+
+const Bar = Class('Bar').extends(Foo, ({ Public, Protected, Private, Super }) => ({
+	constructor(sound: string) {
+	},
+	static: {
+		derivedStatic: 10 
+	},
+	private: {
+		derivedPrivate: 10 
+	},
+	protected: {
+		derivedProtected: 10 
+	},
+	derivedPublicMethod(){
+		Protected(this).protectedMethod("");
+		Protected(this).protectedProp;
+		Protected(this).derivedProtected;
+		Private(this).derivedPrivate;
 		this.test();
+	},
+	test() {
+		return Super(this).test()
 	}
 }));
+
+var bar = new Bar("")
+bar.derivedPublicMethod();
+bar.test()
+Bar.derivedStatic
+
+Bar.staticMethod();
